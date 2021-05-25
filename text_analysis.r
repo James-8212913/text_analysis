98abf4d8fd19e4dfb56dc257a869b1c10f045733
James <james.w.murray@student.uts.edu.au>
2021-05-25 16:11:04 +1000

    updated text analysis - grouped df with counts for each file

--- text_analysis.r
+++ text_analysis.r
@@ -19,6 +19,7 @@ library(tidyverse)
 library(tidytext)
 library(tidymodels)
 library(readtext)
+library(stopwords)
 
 #------------------------------------------------------------------------------#
 # Load Data                                                                  ####
@@ -26,21 +27,56 @@ library(readtext)
 
 file_list <- list.files(path = "." , recursive = TRUE,
                         pattern = ".*.txt",
-                        full.names = TRUE)
+                        full.names = TRUE) 
 
 text_df <- readtext(paste0(file_list))
+text_df # a df with each file as a 'row' agains the file name
 
 # view(text_df)
+text_tidy <- text_df %>% 
+   unnest_tokens(word, text) 
+
+text_tidy
+
+data(stop_words)
+
+text_tidy <- text_tidy %>% 
+   anti_join(stop_words) %>% 
+   group_by(doc_id) %>% 
+   count(word, sort = TRUE) 
+   
+
+text_tidy
+
+plot_list <- text_tidy %>% 
+   group_split(doc_id) 
+   filter(n > 5)
+   map(~ggplot(., aes(n, word)) +
+          geom_col() +
+          labs(y = NULL))
+
+   count(word, sort = TRUE) %>%
+   filter(n >100) %>% 
+   mutate(word = reorder(word, n)) %>% 
+   ggplot(aes(n,word)) +
+   geom_col() +
+   labs(y = NULL)
+
+text_tidy
+
+tidy_text %>% 
+   group_by(doc_id) %>% 
+   count(word, sort = TRUE) %>%
+   filter(n >100) %>% 
+   mutate(word = reorder(word, n)) %>% 
+   ggplot(aes(n,word)) +
+   geom_col() +
+   labs(y = NULL)
 
 # The plan is to analyse each file then consider the topics in each file ----
 
-tidy_text <- text_df %>% 
-  unnest_tokens(word, text)
 
-tidy_text
 
-tidy_text <- tidy_text %>% 
-  anti_join(stop_words)
 tidy_text %>% 
   count(word, sort = TRUE) 
  tidy_text %>% 
