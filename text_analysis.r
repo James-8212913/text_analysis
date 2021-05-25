53f2fec3f059cb09736983bd8212588eaf9b5594
James <james.w.murray@student.uts.edu.au>
2021-05-25 17:21:50 +1000

    EDA Plot list complete with titles for each file

--- text_analysis.r
+++ text_analysis.r
@@ -33,57 +33,30 @@ text_df <- readtext(paste0(file_list))
 text_df # a df with each file as a 'row' agains the file name
 
 # view(text_df)
+
+# Exploratory Analysis on each text file ----
+
+## The text df was broken into tokens in an effor to get a better understanding of how many different word there were and what the counts of each of the words were. Stop_words have been removed as part of the analysis. The filter was set to 8 as part of trail and error - it isn't suitable for all files but is a good start point to sharpen the focus and get a feel for what is in each file. 
+
 text_tidy <- text_df %>% 
-   unnest_tokens(word, text) 
-
-text_tidy
-
-data(stop_words)
-
-text_tidy <- text_tidy %>% 
-   anti_join(stop_words) %>% 
+   unnest_tokens(word, text) %>% 
+   anti_join(stop_words) %>%
    group_by(doc_id) %>% 
-   count(word, sort = TRUE) 
-   
+   count(word,sort = TRUE) %>% 
+   filter(n >8)
 
 text_tidy
 
 plot_list <- text_tidy %>% 
-   group_split(doc_id) 
-   filter(n > 5)
+   group_split() %>% 
    map(~ggplot(., aes(n, word)) +
           geom_col() +
-          labs(y = NULL))
-
-   count(word, sort = TRUE) %>%
-   filter(n >100) %>% 
-   mutate(word = reorder(word, n)) %>% 
-   ggplot(aes(n,word)) +
-   geom_col() +
-   labs(y = NULL)
-
-text_tidy
-
-tidy_text %>% 
-   group_by(doc_id) %>% 
-   count(word, sort = TRUE) %>%
-   filter(n >100) %>% 
-   mutate(word = reorder(word, n)) %>% 
-   ggplot(aes(n,word)) +
-   geom_col() +
-   labs(y = NULL)
-
-# The plan is to analyse each file then consider the topics in each file ----
+          labs(y = NULL,
+               title = .$doc_id))
+# View the Plots for each file as part of EDA
+plot_list
 
 
 
-tidy_text %>% 
-  count(word, sort = TRUE) 
- tidy_text %>% 
-   count(word, sort = TRUE) %>%
-   filter(n >100) %>% 
-   mutate(word = reorder(word, n)) %>% 
-   ggplot(aes(n,word)) +
-   geom_col() +
-   labs(y = NULL)
- 
+
+
